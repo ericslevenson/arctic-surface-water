529e9573f43d09776dab09bc4deb272bdd9375f2
Eric Levenson <60052624+ericslevenson@users.noreply.github.com>
2023-11-29 21:34:50 -0500

    updated batch processing

--- OccurrenceProcessing.py
+++ OccurrenceProcessing.py
@@ -5,7 +5,9 @@ Created on Thu Jul  6 16:23:48 2023
 
 @author: ericlevenson
 
-description: Clean up Lake Occurrence Rasters with a mask. based on segmentation
+description: Re-trying lake occurrence sans buffer
+    
+Clean up Lake Occurrence Rasters with a mask. based on segmentation
 techniques that separate the foreground from background. Dilating the 
 segmented image produces a useful mask to filter the original image.
 """
@@ -27,23 +29,22 @@ from rasterio.features import shapes
 from shapely.geometry import shape
 
 
-
-
 ## ***INPUTS***
 user = 'ericlevenson' # ericlevenson = laptop; elevens2 = desktop
+utmzone = '3N'
+vectors = False
 mode = 'batch'  ### single or batch for one image or entire folder
-background_threshold, foreground_threshold = 0.25, 0.3 # markers for watershed segmentation
-directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeTrack/lakeOccurrence/5N/unprocessed_originals/' # Path to lakeOccurrence Folder
+background_threshold = 10
+foreground_threshold = 75 # markers for watershed segmentation
+directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeScan/postProcessing/{utmzone}/unprocessed_combined/' # Path to lakeOccurrence Folder
 tile_id =  342 # use in single mode
-raster_out_directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeTrack/lakeOccurrence/5N/processed/' # Path to processed lake Occurrence Folder
-shapefile_out_directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeTrack/lakeOccurrence/5N/vectors/segmented/'
-buffered_shapefile_out_directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeTrack/lakeOccurrence/5N/vectors/expanded/'
-input_descriptor = '_5N_lakeOccurrence_2016-2021.tif'
-output_descriptor = '_5N_lakeOccurrence_2016-2021_processed.tif'
-
+raster_out_directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeScan/postProcessing/{utmzone}/rasters_{str(background_threshold)}-{str(foreground_threshold)}_new/' # Path to processed lake Occurrence Folder
+shapefile_out_directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeScan/postProcessing/{utmzone}/vectors/segmented_{str(background_threshold)}-{str(foreground_threshold)}_new/'
+buffered_shapefile_out_directory = f'/Users/{user}/Dropbox (University of Oregon)/ArcticLakeScan/postProcessing/{utmzone}/vectors/expanded/'
+input_descriptor = f'{utmzone}_lakeOccurrence_2016-2021.tif'
+output_descriptor = f'_{utmzone}_lakeOccurrence_2016-2021_processed_{str(background_threshold)}-{str(foreground_threshold)}.tif'
 
 ## ***Methods***
-
 def im_process(image, background_threshold, foreground_threshold):
     '''Perform image processing on numpy array input'''
     # Edge detection
@@ -59,18 +60,16 @@ def im_process(image, background_threshold, foreground_threshold):
     ws = watershed(edges, markers)
     # Label Segments
     seg1 = label(ws == foreground)
-    # Dilate Labels
-    expanded = expand_labels(seg1, distance=6)
-    # Convert 'expanded' to a boolean mask
-    expanded_mask = expanded.astype(bool)
+    # Convert seg1 to a boolean mask
+    boolmask = seg1.astype(bool)
     # Apply the boolean mask to the 'image' variable
-    image[~expanded_mask] = np.nan
+    image[~boolmask] = np.nan
     # Binarize segmented and expanded
     seg1 = np.where(seg1 == 0, 0, 1).astype(np.int32)
-    expanded = np.where(expanded == 0, 0, 1).astype(np.int32)
-    return image, seg1, expanded
+    #expanded = np.where(expanded == 0, 0, 1).astype(np.int32)
+    return image, seg1
 
-def vectorize(segmented, expanded):
+def vectorize(segmented):
     '''Produces geodataframes for a segmented and expanded segmentation raster'''
     shape_gen = ((shape(s), v) for s, v in shapes(segmented, transform=dataset.transform))
     # build a pd.DataFrame of shapes and convert to geodataframe
@@ -79,12 +78,12 @@ def vectorize(segmented, expanded):
     # filter out non-lake polygons
     gdf_seg = gdf_seg[gdf_seg['class'] == 1]
     del shape_gen
-    shape_gen = ((shape(s), v) for s, v in shapes(expanded, transform=dataset.transform))
-    df_expand = DataFrame(shape_gen, columns=['geometry', 'class'])
-    gdf_expand = GeoDataFrame(df_expand["class"], geometry=df_expand.geometry, crs=dataset.crs)
+    #shape_gen = ((shape(s), v) for s, v in shapes(expanded, transform=dataset.transform))
+    #df_expand = DataFrame(shape_gen, columns=['geometry', 'class'])
+    #gdf_expand = GeoDataFrame(df_expand["class"], geometry=df_expand.geometry, crs=dataset.crs)
     # filter out non-lake polygons
-    gdf_expand = gdf_expand[gdf_expand['class'] == 1]
-    return gdf_seg, gdf_expand
+    #gdf_expand = gdf_expand[gdf_expand['class'] == 1]
+    return gdf_seg
     
 def visualize_processing(image, background_threshold, foreground_threshold):
     ''''Process image and produce plot for intermediate steps'''
@@ -127,7 +126,7 @@ def visualize_processing(image, background_threshold, foreground_threshold):
 if __name__ == "__main__":
     if mode == 'single':
         # Create image path
-        impath = str(directory + str(tile_id) + '_5N_lakeOccurrence_2016-2021.tif')           
+        impath = str(directory + str(tile_id) + '_{utmzone}_lakeOccurrence_2016-2021.tif')           
         # open image and record metadata
         with rasterio.open(impath, dtype='float64') as dataset:
             # Access the image data
@@ -136,17 +135,17 @@ if __name__ == "__main__":
             meta = dataset.meta
             meta.update(compress='lzw') # reduce file size
             # Segment image
-            image, segmented, expanded = im_process(image, background_threshold, foreground_threshold)
+            image, segmented = im_process(image, background_threshold/100, foreground_threshold/100)
             # Write the modified 'image' variable to a new raster file
             export_descriptor = str(str(tile_id) + output_descriptor)
             with rasterio.open(str(raster_out_directory + export_descriptor), 'w', **meta) as dst:
                 dst.write(image, 1)
             # Vectorize
-            gdf_seg, gdf_expand = vectorize(segmented, expanded)
+            gdf_seg = vectorize(segmented)
             # Write the lake shapefile
             gdf_seg.to_file(shapefile_out_directory + str(tile_id) + '.shp')
             # Write the buffered lake shapefile
-            gdf_expand.to_file(buffered_shapefile_out_directory + str(tile_id) + '_buffered.shp')
+            #gdf_expand.to_file(buffered_shapefile_out_directory + str(tile_id) + '_buffered.shp')
     elif mode == 'batch':
         processed = [i.split('_')[0] for i in os.listdir(raster_out_directory) if output_descriptor in i] # list of processed tiles
         images = [i for i in os.listdir(directory) if input_descriptor in i and i.split('_')[0] not in processed] # get list of images
@@ -161,18 +160,19 @@ if __name__ == "__main__":
                 meta = dataset.meta
                 meta.update(compress='lzw')
                 # Segment image
-                image, segmented, expanded = im_process(image, background_threshold, foreground_threshold)
+                image, segmented = im_process(image, background_threshold/100, foreground_threshold/100)
                 # Write the modified 'image' variable to a new raster file
                 export_descriptor = str(str(i.split('_')[0] + output_descriptor))
                 with rasterio.open(str(raster_out_directory + export_descriptor), 'w', **meta) as dst:
                     dst.write(image, 1)
             # Vectorize
-            gdf_seg, gdf_expand = vectorize(segmented, expanded)
-            # Write the lake shapefile
-            gdf_seg.to_file(shapefile_out_directory + str(i.split('_')[0]) + '.shp')
-            # Write the buffered lake shapefile
-            gdf_expand.to_file(buffered_shapefile_out_directory + str(i.split('_')[0]) + '_buffered.shp')
+            if vectors == True:
+                gdf_seg = vectorize(segmented)
+                # Write the lake shapefile
+                gdf_seg.to_file(shapefile_out_directory + f'{utmzone}_' + str(i.split('_')[0]) + '.shp')
+            else:
+                continue
         print('Well, that was fun!')
     else:
         print('ERROR: Set mode input variable to single or batch')
-        
+    
